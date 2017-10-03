46cffdfaa6e856c693a1f12c1f86ccd17ab706f0
Xincheng Yang <xyang76@hawk.iit.edu>
2017-10-03 10:59:06 -0500

    new

--- hw2/main.c
+++ hw2/main.c
@@ -11,8 +11,6 @@ void Test(int n) {
 
 int main(int argc,char* argv[]) {
     #pragma omp parallel for
-    for ( int j = 0; j < 4; j++ )
-    {
-        printf("j = %d, ThreadId = %d\n", j, omp_get_thread_num());
-    }
+    for(int i = 0; i < 10; ++i)
+        Test( i );
 }
