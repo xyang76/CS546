60b5d05f173d276e5f78dc9697406a31043ad068
Xincheng Yang <xyang76@hawk.iit.edu>
2017-10-03 10:48:48 -0500

    new

---
+++ hw2/main.c
@@ -0,0 +1,17 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void Test(int n) {
+    for(int i = 0; i < 10000; ++i) {
+    //do nothing, just waste time
+    }
+    printf("%d, ", n);
+}
+
+int main(int argc,char* argv[]) {
+    #pragma omp parallel for
+    for(int i = 0; i < 10; ++i)
+        Test( i );
+    system("pause");
+}
